5f0f59063be0fea62ad1a5d742775e20244d8930
mayconds000 <mayconds000@gmail.com>
2019-04-19 11:18:35 -0300

    add store

--- src/store.js
+++ src/store.js
@@ -5,12 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    list: []
   },
   mutations: {
-
+    UPDATE_TASK (state, task) {
+      state.list = state.list.map(item => {
+        if (item.id == task.id ) item = task
+        return item
+      })
+    },
+    DELETE_TASK (state, task) {
+      state.list = state.list.filter(item => item.id != task.id)
+    }
   },
   actions: {
-
+    updateTask ({commit}, task) {
+      commit('UPDATE_TASK', task);
+    },
+    deleteTask ({commit}, task) {
+      commit('DELETE_TASK', task);
+    }
   }
 })
